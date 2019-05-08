244a5123b8d08f63d04bfd3c6c5714d052ca195d
bhubea <Gelbfisch11>
2019-05-08 14:25:19 +0200

    Datatype Change
    Mit diesen Files kann man die falschen Dtaentypen in der DB ändern

---
+++ Datatype Change/Change Datatype Charakters.sql
@@ -0,0 +1,19 @@
+use Gehirnjogging;
+Alter Table Charakters Alter Column HP int;
+GO
+
+Alter Table Charakters Alter Column Damage int;
+GO
+
+Alter Table Charakters Alter Column Luck int;
+GO
+
+Alter Table Charakters Alter Column Stage int;
+GO
+
+Alter Table Charakters Alter Column SolveTime int;
+GO
+
+Alter Table Charakters Alter Column Assets int;
+GO
+
---
+++ Datatype Change/Change Datatype PassedLevels.sql
@@ -0,0 +1,5 @@
+USE Gehirnjogging;
+GO
+
+ALTER TABLE PassedLevels ALTER Column Time INT NOT NULL;
+GO
