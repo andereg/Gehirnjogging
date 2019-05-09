15ff534ecae3ac63c70964b6aebe7e3b794b0d0b
bhubea <Gelbfisch11>
2019-05-09 13:55:25 +0200

    Files Säuberung
    unnötige Files wurden aus dem rep Ordner entfernt

--- Datatype Change/Change Datatype PassedLevels.sql
+++
@@ -1,5 +0,0 @@
-USE Gehirnjogging;
-GO
-
-ALTER TABLE PassedLevels ALTER Column Time INT NOT NULL;
-GO
--- Datatype Change/Change Datatype Questions.sql
+++
@@ -1,5 +0,0 @@
-USE Gehirnjogging;
-GO
-
-ALTER TABLE Questions ALTER Column Answer INT NOT NULL;
-GO
