5c504f3c730e09781a47055327ab1270b612aaef
Anderegg Raphael <bander@bbc-be.local>
2019-05-09 17:04:36 +0200

    Spielstand laden Layout hinzugefügt
    Die Datenbanklogik und das effektive Spielladen muss noch gemacht werden

--- GehirnJogging/GehirnJogging/Startpage.xaml.cs
+++ GehirnJogging/GehirnJogging/Startpage.xaml.cs
@@ -26,6 +26,8 @@ namespace GehirnJogging
     {
 
         private Player _player = Player.GetInstance();
+        private int _scrollValue = 1;
+        private int _maxButtons = 10;
 
         /// <summary>
         /// Nach einem Navigieren auf diese Seite, löst es ein neues Event aus, welche die private Methode OnNavigated ausführt
@@ -82,12 +84,73 @@ namespace GehirnJogging
 
         private void BtnloadGame_Click(object sender, RoutedEventArgs e)
         {
+            int marginTop = 333;
             GridLoadGame.Visibility = Visibility.Visible;
+            for (int i = 1; i < _maxButtons; i++)
+            {
+                if (i > 5)
+                {
+                    this.GridSpielstandButtons.Children.Add(new Button { HorizontalAlignment = HorizontalAlignment.Left, Tag = i, FontSize = 30, Content = "Spielstand " + i, FontWeight = FontWeights.Bold, Background = Brushes.LightYellow, BorderBrush = Brushes.Black, BorderThickness = new Thickness(8), Margin = new Thickness(585, marginTop, 0, 0),Visibility = Visibility.Hidden ,VerticalAlignment = VerticalAlignment.Top, Width = 765, Height = 74 });
+                }
+                else this.GridSpielstandButtons.Children.Add(new Button { HorizontalAlignment = HorizontalAlignment.Left, Tag = i, FontSize = 30, Content = "Spielstand " + i ,FontWeight = FontWeights.Bold, Background = Brushes.LightYellow, BorderBrush = Brushes.Black, BorderThickness = new Thickness(8), Margin = new Thickness(585, marginTop, 0, 0), VerticalAlignment = VerticalAlignment.Top, Width = 765, Height = 74 });
+                marginTop = marginTop + 80;
+            }
         }
 
         private void ExitLoadGame_Click(object sender, RoutedEventArgs e)
         {
             GridLoadGame.Visibility = Visibility.Hidden;
+        }
+
+        private void ArrowUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (_scrollValue == 1)
+            {
+                return;
+            }
+            foreach (Button spielstand in GridSpielstandButtons.Children)
+            {
+                if (spielstand.Tag.GetHashCode() == _scrollValue - 1)
+                {
+                    spielstand.Visibility = Visibility.Visible;
+                }
+                if (spielstand.Tag.GetHashCode() == _scrollValue + 4)
+                {
+                    spielstand.Visibility = Visibility.Hidden;
+                }
+            }
+            Thickness GridSpielstandButtonsmarginTop = GridSpielstandButtons.Margin;
+            GridSpielstandButtonsmarginTop.Top = GridSpielstandButtonsmarginTop.Top + 80;
+            GridSpielstandButtons.Margin = GridSpielstandButtonsmarginTop;
+
+            _scrollValue--;
+
+        }
+
+        private void ArrowDown_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (_scrollValue + 5 == _maxButtons)
+            {
+                return;
+            }
+
+            foreach (Button spielstand in GridSpielstandButtons.Children)
+            {
+                if (spielstand.Tag.GetHashCode() == _scrollValue)
+                {
+                    spielstand.Visibility = Visibility.Hidden;
+                }
+                if (spielstand.Tag.GetHashCode() == _scrollValue + 5)
+                {
+                    spielstand.Visibility = Visibility.Visible;
+                }
+            }
+            Thickness GridSpielstandButtonsmarginTop = GridSpielstandButtons.Margin;
+            GridSpielstandButtonsmarginTop.Top = GridSpielstandButtonsmarginTop.Top - 80;
+            GridSpielstandButtons.Margin = GridSpielstandButtonsmarginTop;
+
+            _scrollValue++;
         }
     }
 }
